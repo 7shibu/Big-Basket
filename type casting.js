2fdd59c276e8cf1e1d9b02e48b93ec5e5e0fb989
7shibu <shibu.mastana89@gmail.com>
2022-03-28 23:53:10 +0530

    No changes

---
+++ type casting.js
@@ -0,0 +1,9 @@
+console.log("This Is Type Casting Exmaple");
+let myNum= 34;
+myNum=String(34);
+console.log(myNum ,(typeof myNum));
+let booleanVar=String(true);
+console.log(booleanVar,(typeof booleanVar));
+
+let arr=String([1,2,3,4,5]);
+console.log(arr.length,(typeof arr));
